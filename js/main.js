4cf407f5c3f7182f5ad3bbde5d1d5aaa4e9dde6c
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-04-07 22:08:23 +0900

    adjust

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ var group;
 var mouseX = 0,
     mouseY = 0;
 
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
+var WIDTH = 640;
+var HEIGHT = 350;
 
 exports.default = {
   init: init,
@@ -23,7 +23,7 @@ function init() {
 
   container = document.getElementById('container');
 
-  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 2000);
+  camera = new THREE.PerspectiveCamera(60, WIDTH / HEIGHT, 1, 2000);
   camera.position.z = 500;
 
   scene = new THREE.Scene();
@@ -50,12 +50,6 @@ function init() {
   canvas.height = 128;
 
   var context = canvas.getContext('2d');
-  var gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
-  gradient.addColorStop(0.1, 'rgba(210,210,210,1)');
-  gradient.addColorStop(1, 'rgba(255,255,255,1)');
-
-  context.fillStyle = gradient;
-  context.fillRect(0, 0, canvas.width, canvas.height);
 
   var texture = new THREE.CanvasTexture(canvas);
 
@@ -68,41 +62,16 @@ function init() {
   group.add(mesh);
 
   renderer = new THREE.CanvasRenderer();
-  renderer.setClearColor(0xffffff);
+  renderer.setClearColor(0x000000);
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(WIDTH, HEIGHT);
   container.appendChild(renderer.domElement);
 
   stats = new Stats();
   stats.domElement.style.position = 'absolute';
   stats.domElement.style.top = '0px';
   container.appendChild(stats.domElement);
-
-  document.addEventListener('mousemove', onDocumentMouseMove, false);
-
-  //
-
-  window.addEventListener('resize', onWindowResize, false);
 }
-
-function onWindowResize() {
-
-  windowHalfX = window.innerWidth / 2;
-  windowHalfY = window.innerHeight / 2;
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function onDocumentMouseMove(event) {
-
-  mouseX = event.clientX - windowHalfX;
-  mouseY = event.clientY - windowHalfY;
-}
-
-//
 
 function animate() {
 
--- src/js/Earth.js
+++ src/js/Earth.js
@@ -3,8 +3,8 @@ var camera, scene, renderer;
 var group;
 var mouseX = 0, mouseY = 0;
 
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
+var WIDTH = 640;
+var HEIGHT = 350;
 
 export default {
   init: init,
@@ -15,7 +15,7 @@ function init() {
 
   container = document.getElementById( 'container' );
 
-  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 2000 );
+  camera = new THREE.PerspectiveCamera( 60, WIDTH / HEIGHT, 1, 2000 );
   camera.position.z = 500;
 
   scene = new THREE.Scene();
@@ -43,19 +43,6 @@ function init() {
   canvas.height = 128;
 
   var context = canvas.getContext( '2d' );
-  var gradient = context.createRadialGradient(
-    canvas.width / 2,
-    canvas.height / 2,
-    0,
-    canvas.width / 2,
-    canvas.height / 2,
-    canvas.width / 2
-  );
-  gradient.addColorStop( 0.1, 'rgba(210,210,210,1)' );
-  gradient.addColorStop( 1, 'rgba(255,255,255,1)' );
-
-  context.fillStyle = gradient;
-  context.fillRect( 0, 0, canvas.width, canvas.height );
 
   var texture = new THREE.CanvasTexture( canvas );
 
@@ -68,9 +55,9 @@ function init() {
   group.add( mesh );
 
   renderer = new THREE.CanvasRenderer();
-  renderer.setClearColor( 0xffffff );
+  renderer.setClearColor( 0x000000 );
   renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize( WIDTH, HEIGHT );
   container.appendChild( renderer.domElement );
 
   stats = new Stats();
@@ -78,34 +65,7 @@ function init() {
   stats.domElement.style.top = '0px';
   container.appendChild( stats.domElement );
 
-  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
-  //
-
-  window.addEventListener( 'resize', onWindowResize, false );
-
 }
-
-function onWindowResize() {
-
-  windowHalfX = window.innerWidth / 2;
-  windowHalfY = window.innerHeight / 2;
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-function onDocumentMouseMove( event ) {
-
-  mouseX = ( event.clientX - windowHalfX );
-  mouseY = ( event.clientY - windowHalfY );
-
-}
-
-//
 
 function animate() {
 
