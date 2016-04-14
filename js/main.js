acb0c918c962a48435b1ab2a53cf4670505f0cd3
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-04-14 23:03:30 +0900

    build

--- js/main.js
+++ js/main.js
@@ -3809,7 +3809,7 @@ var Main = function () {
       this.earth.animate();
       this.movie = new _Movie2.default();
 
-      var html = $('.template--com__youtube').text();
+      var html = $('.template--jp__co__yahoo__headlines').text();
       $('.bg-site').html(html);
     }
   }, {
